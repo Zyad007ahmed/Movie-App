85901c27988df4d6e7435a04ac31b27475c9bc30
Zyad Ahmed <Zyad.ahmed.Za55@gmail.com>
2025-11-12 23:51:11 +0200

    add assets (icon), env class, create exceptions and fauilers, create constants of colors , strings and values and some of core files

---
+++ lib/core/config/env.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Env{
+  static String get apiKey => _get('API_KEY');
+  static String get baseUrl => _get('API_BASE_URL');
+
+  static String _get(String key){
+    final value = dotenv.env[key];
+    if(value == null || value.isEmpty){
+      throw Exception("Env key $key not found");
+    }
+    return value;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await loadEnvVariables();
+
   runApp(const MyApp());
+}
+
+Future<void> loadEnvVariables() async {
+  await dotenv.load();
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +23,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: 
     );
   }
 }
